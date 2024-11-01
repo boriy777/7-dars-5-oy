fed14869d46f1b7a0e184b2ecbc1a0b5291b82ba
boriy777 <abdulboriy822@gmail.com>
2024-11-01 19:07:37 +0500

    Add files via upload

---
+++ scripe.js
@@ -0,0 +1,22 @@
+
+let myArray = ["anor", "nok", "uzum", "shoptoli"]
+
+console.log("array:", myArray);
+
+alert("Array :" + myArray);
+
+if(confirm("Arraydan bitta element qoshilishini xohlaysizmi?")){
+
+    myArray.push('banan')
+
+
+}else{
+    console.log(myArray.pop)
+}
+    
+
+
+
+console.log("yangilagan array", myArray);
+
+alert("yangilangan array : " + myArray)
